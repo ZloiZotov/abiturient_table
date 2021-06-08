a293234f978a6a5d13b7887b36047d0fac1a5225
ZloiZotov <zloizotov@gmail.com>
2021-06-08 16:19:05 +0400

    Update script_table.js

--- script_table.js
+++ script_table.js
@@ -1,31 +1,29 @@
-
-
 var table = {
     table1: {
-        sz_b: 1, sz_b_copy: 1, sz_c: 1, sz_c_copy: 4,
-        u_sz_b: 5, u_sz_b_copy: 6,
-                                sz_b_11: 7, sz_b_11_copy: 8,
-        sm_b: 9, sm_b_copy: 10,
-        zo_b: 11, zo_b_copy: 12, zo_c: 13, zo_c_copy: 14,
-        er_b: 15, er_b_copy: 16,
-        sp_b: 17, sp_b_copy: 18,
-        bk_b: 19, bk_b_copy: 20, bk_c: 21, bk_c_copy: 22,
-        t1_itog1: 1, t1_itog2: 2, t1_itog3: 3, t1_itog4: 4
+        sz_b: 0, sz_b_copy: 0, sz_c: 0, sz_c_copy: 0,
+        u_sz_b: 0, u_sz_b_copy: 0,
+                                sz_b_11: 0, sz_b_11_copy: 0,
+        sm_b: 0, sm_b_copy: 0,
+        zo_b: 1, zo_b_copy: 0, zo_c: 1, zo_c_copy: 0,
+        er_b: 0, er_b_copy: 0,
+        sp_b: 0, sp_b_copy: 0,
+        bk_b: 0, bk_b_copy: 0, bk_c: 0, bk_c_copy: 0,
+        t1_itog1: 0, t1_itog2: 0, t1_itog3: 0, t1_itog4: 0
 
     },
     table2: {
-        z_sz_b: 1, z_sz_b_copy: 1, z_sz_c: 1, z_sz_c_copy: 1,
-        z_sm_b: 2, z_sm_b_copy: 2, z_sm_c: 2, z_sm_c_copy: 2,
-        z_zo_b: 3, z_zo_b_copy: 3, z_zo_c: 3, z_zo_c_copy: 3,
-                                   z_er_c: 4, z_er_c_copy: 4,
-        t2_itog1:5, t2_itog2: 5, t2_itog3:5, t2_itog4: 5,
+        z_sz_b: 0, z_sz_b_copy: 0, z_sz_c: 0, z_sz_c_copy: 0,
+        z_sm_b: 3, z_sm_b_copy: 0, z_sm_c: 0, z_sm_c_copy: 0,
+        z_zo_b: 0, z_zo_b_copy: 0, z_zo_c: 0, z_zo_c_copy: 0,
+                                   z_er_c: 0, z_er_c_copy: 0,
+        t2_itog1: 0, t2_itog2: 0, t2_itog3:0, t2_itog4: 0,
 
     },
   };
   var json = JSON.stringify(table);
   var data = JSON.parse(json);
 
-for (var i in data.table1) {
+  for (var i in data.table1) {
     document.getElementById(i).innerHTML = data.table1[i];
   }
 
